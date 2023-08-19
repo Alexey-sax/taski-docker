5301066dcc4a3b879e8a8011bdf5a0f430785bce
Alexey-sax <122888153+Alexey-sax@users.noreply.github.com>
2023-08-19 09:07:56 +0400

    Update 0001_initial.py

--- backend/api/migrations/0001_initial.py
+++ backend/api/migrations/0001_initial.py
@@ -15,9 +15,9 @@ class Migration(migrations.Migration):
             name='Task',
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True,
-                                           serialize=False, verbose_name='ID')),
+                                          serialize=False, verbose_name='ID')),
                 ('title', models.CharField(max_length=120,
-                                verbose_name='Заголовок')),
+                               verbose_name='Заголовок')),
                 ('description', models.TextField()),
                 ('completed', models.BooleanField(default=False)),
             ],
